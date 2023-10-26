0e618d1ce9bf761595790678994db5a08ba14b20
Juan Pablo <jplandi@utpl.edu.ec>
2023-10-26 17:54:35 -0500

    Este ejercicio lo resolvió jp

---
+++ Ejercicio03/Ejercicio.03/src/ejercicio/pkg03/Ejercicio03.java
@@ -0,0 +1,17 @@
+package ejercicio.pkg03;
+
+import sun.misc.Signal;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+        int var1 = 10;
+        int var2 = 16;
+        double var3= 20;
+        int var4= 12;
+        double respuesta;
+        respuesta = Math.pow(var1,2) + Math.sqrt(var2) + var3/var4;
+        System.out.println("La respuesta es: " + respuesta);
+        
+    }
+    
+}
