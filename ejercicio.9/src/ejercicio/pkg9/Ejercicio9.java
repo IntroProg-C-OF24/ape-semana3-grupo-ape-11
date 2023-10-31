60246a8e00f5b010d544b3b35a5bbc33e55fd7b9
Juan Pablo <jplandi@utpl.edu.ec>
2023-10-30 20:01:39 -0500

    Este ejercicio fue resuelto por el grupo 11

---
+++ ejercicio.9/src/ejercicio/pkg9/Ejercicio9.java
@@ -0,0 +1,15 @@
+package ejercicio.pkg9;
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        int var1 = 81;
+        int var2 = 25;
+        int var3 = 10;
+        int var4 = 50;
+        double var5 = (Math.sqrt(var1) + (Math.sqrt(var2) * var3));
+        boolean respuesta;
+        respuesta = var5 >= var4;
+        System.out.println("La operacion da como resultado: " + respuesta);
+        //ESTE EJERCICIO LO REALIZO JUAN PABLO
+    }
+    
+}
